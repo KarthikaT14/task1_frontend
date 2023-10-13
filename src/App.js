80b9e3d9adc87d079c7664eae60208527d3fb412
Siddhartha <siddupaturi09@gmail.com>
2023-10-13 12:10:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WordCounter from 'F:/REACTJS_S/frontend1/src/components/wordcounter.js';
+import 'F:/REACTJS_S/frontend1/src/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WordCounter/>
+    </div>
+  );
+}
+
+export default App;
